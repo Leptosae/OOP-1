4da0b77ad82aad5675a1c04772f2f615f3b798c0
Steven Edwards <stevenaedwards97@gmail.com>
2018-01-28 18:51:16 -0500

    main.cpp
    card class and enum suit and rank

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+enum Rank {
+  Ace,
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+};
+
+enum Suit {
+  Hearts, 
+  Diamonds,
+  Clubs, 
+  Spades,
+};
+
+//typed up but essentially copied from your notes. 
+
+
+
+Class Card{
+
+Rank rank;
+Suit suit;
+
+
+Card(Rank r, Suit s)
+    : rank(r), suit(s)
+
+  
+};
+
+
+int main()
+{
+  
+}//
